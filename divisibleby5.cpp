d5e2558096f2994afaac4a1a54780e27f29a37f2
NITIN DHURVE <161688626+nitingithub12@users.noreply.github.com>
2024-03-02 10:08:13 +0530

    Add files via upload

---
+++ divisibleby5.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a;
+	cout<<"enter positive number here:";
+	cin>>a;
+	if(a%5==0){
+		cout<<"divisible by 5";
+		
+	}
+	else
+	cout<<"not divisible by 5";
+}
