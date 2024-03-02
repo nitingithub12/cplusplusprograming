908b66481ee76772296f814ec7753e7c01d51161
NITIN DHURVE <161688626+nitingithub12@users.noreply.github.com>
2024-03-02 10:04:09 +0530

    Add files via upload

---
+++ checkdigit.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int num=0;
+	cout<<"take a integer:";
+	cin>>num;
+	if(num>99 && num <1000){
+		cout<<"three digit number :";
+	}
+	else
+	cout<<"not three digit number :";
+	
+}
