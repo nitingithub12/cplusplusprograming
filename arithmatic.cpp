5ddae2acdf381bc93c8de6be12c95f409dfdb6b0
NITIN DHURVE <161688626+nitingithub12@users.noreply.github.com>
2024-03-02 10:02:44 +0530

    Add files via upload

---
+++ arithmatic.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a=8,b=6,sum,mul,subs,divi;
+	sum=a+b;
+	cout<<"sum =";
+	cout<<sum;
+	cout<<endl;
+	mul=a*b;
+	cout<<"mul =";
+	cout<<mul;
+	cout<<endl;
+	subs=a-b;
+	cout<<"substraction =";
+	cout<<subs;
+	cout<<endl;
+	divi=a/b;
+	cout<<"divi =";
+	cout<<divi;
+	
+}
